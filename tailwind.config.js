3d5cf3ef36ac6e225261861495764c7f466380de
Kamil Romanowicz <kamil.romanowicz95@gmail.com>
2024-11-07 11:37:41 +0100

    tailwind fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
-// tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: media, // or 'media' or 'class'
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+
+      },
+    },
   },
-  variants: {},
   plugins: [],
-}
+};
